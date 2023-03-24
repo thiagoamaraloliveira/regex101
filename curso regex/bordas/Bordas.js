a6446906760243e951b6d9beb67d1969ca43570a
thiago.ao <thiago.ao@samsung.com>
2023-03-24 09:03:28 -0300

    Bordas

---
+++ curso regex/bordas/Bordas.js
@@ -0,0 +1,6 @@
+const texto = "Romário era um excelente jogador\n, mas hoje é um político questionador"
+
+console.log(texto.match(/r/gi))
+console.log(texto.match(/^r/gi)) // ^ inicion da linha/string
+console.log(texto.match(/r$/gi)) // $ fim da linha/string
+console.log(texto.match(/^r.*r$/gi)) // problema do dotall - .* não reconhece \n
